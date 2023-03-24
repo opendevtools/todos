5754c83e140e42b527ab47acc7a0f40ea14ef754
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-03-24 20:04:00 +0100

    feat: add filtering

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 mod models;
 
-use colored::*;
+use colored::Colorize;
 use jwalk::WalkDir;
 use models::{todo::Todo, todo_type::contains_todo_type};
-use std::{collections::HashMap, fs};
+use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = std::env::args().nth(1);
     let needle = std::env::args().nth(2);
 
+    // Display help if no path is provided
     if path.is_none() {
         println!("Usage: todo <path>");
         return Ok(());
     }
 
-    let path = path.unwrap();
+    let mut files_scanned = 0;
     let mut ok_files = 0;
     let mut filtered_files = 0;
-    let mut todos: HashMap<String, Vec<Todo>> = HashMap::new();
+    let mut todos: Vec<Todo> = vec![];
+
+    let path = path.unwrap();
     let supported_filetypes = vec!["ts", "js", "tsx", "jsx", "vue", "html"];
 
     for entry in WalkDir::new(&path).sort(true) {
         let entry = entry?;
+        files_scanned += 1;
 
         // Skip directories
         if entry.file_type().is_dir() {
@@ -36,12 +40,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Read the file contents
         let file_contents = fs::read_to_string(entry.path())?;
 
-        // Skip files without TODOs
-        // if !vec!["//", "<!--"].contains(&file_contents) {
-        //     ok_files += 1;
-        //     continue;
-        // }
+        // Skip files without comments
+        if !file_contents.contains("//") && !file_contents.contains("<!--") {
+            ok_files += 1;
+            continue;
+        }
 
+        // Check and parse todos
         for line in file_contents.lines().enumerate() {
             if contains_todo_type(line.1).is_ok() {
                 if needle.is_some() && !line.1.contains(needle.as_ref().unwrap()) {
@@ -49,37 +54,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     continue;
                 }
 
-                todos
-                    .entry(entry.path().display().to_string())
-                    .or_default()
-                    .push(line.into());
+                let todo = (line.0, line.1, entry.path().display().to_string());
+
+                todos.push(todo.into());
             }
         }
     }
 
-    match (ok_files, filtered_files) {
-        (0, 0) => println!("No TODOs found"),
-        (0, 1) => println!("1 file filtered"),
-        (1, 0) => println!("1 file OK"),
-        (1, 1) => println!("1 file OK / 1 file filtered"),
-        (_, 0) => println!("{} files OK", ok_files),
-        (_, 1) => println!("{} files OK / 1 file filtered", ok_files),
-        (0, _) => println!("{} files filtered", filtered_files),
-        (1, _) => println!("1 file OK / {} files filtered", filtered_files),
-        (_, _) => println!("{} files OK / {} files filtered", ok_files, filtered_files),
-    }
+    // Print the results
+    todos.iter().for_each(|todo| println!("{todo}"));
 
-    println!("");
-
-    for (filename, todos) in todos {
-        for todo in todos {
-            println!(
-                "{} {}",
-                todo,
-                filename.replace(&path, "").bold().bright_black(),
-            );
-        }
-    }
+    println!(
+        "\n{} {}\n{} {} scanned {} {} OK {} {} filtered",
+        "Todos:".bold(),
+        todos.len().to_string().blue(),
+        "Files:".bold(),
+        files_scanned.to_string().yellow(),
+        "/".bright_black(),
+        ok_files.to_string().green(),
+        "/".bright_black(),
+        filtered_files.to_string().red(),
+    );
 
     Ok(())
 }
--- src/models/todo.rs
+++ src/models/todo.rs
@@ -4,12 +4,13 @@ use nom::{bytes::complete::take_till1, character::complete, IResult};
 use std::fmt::Display;
 
 pub struct Todo {
+    file_path: String,
     line_number: (usize, usize),
     text: String,
     todo_type: TodoType,
 }
 
-fn parse_todo(input: &str, line_number: usize) -> IResult<&str, Todo> {
+fn parse_todo(input: &str, line_number: usize, file_path: String) -> IResult<&str, Todo> {
     let (input, column) = complete::multispace0(input)?;
     let (input, todo_type) = contains_todo_type(input)?;
     let (input, _) = take_till1(char::is_alphabetic)(input)?;
@@ -17,16 +18,17 @@ fn parse_todo(input: &str, line_number: usize) -> IResult<&str, Todo> {
     Ok((
         input,
         Todo {
+            file_path,
             line_number: (line_number + 1, column.len() + 1),
-            text: input.replace(" -->", "").to_string(),
+            text: input.replace("-->", "").trim().to_string(),
             todo_type: todo_type.into(),
         },
     ))
 }
 
-impl From<(usize, &str)> for Todo {
-    fn from((line_number, text): (usize, &str)) -> Self {
-        parse_todo(text, line_number).unwrap().1
+impl From<(usize, &str, String)> for Todo {
+    fn from((line_number, text, path): (usize, &str, String)) -> Self {
+        parse_todo(text, line_number, path).unwrap().1
     }
 }
 
@@ -37,7 +39,11 @@ impl Display for Todo {
             "{} {} {}",
             self.todo_type,
             self.text,
-            format!("[{}:{}]", self.line_number.0, self.line_number.1).bright_black()
+            format!(
+                "[{}:{}:{}]",
+                self.file_path, self.line_number.0, self.line_number.1
+            )
+            .bright_black()
         )
     }
 }
@@ -48,7 +54,7 @@ mod tests {
 
     #[test]
     fn test_todo_from() {
-        let todo = Todo::from((0, "  // TODO: This is a todo"));
+        let todo = Todo::from((0, "  // TODO: This is a todo", "test.rs".to_string()));
 
         assert_eq!(todo.line_number, (1, 3));
         assert_eq!(todo.text, "This is a todo");
@@ -57,7 +63,7 @@ mod tests {
 
     #[test]
     fn test_todo_from_with_other_delimiter() {
-        let todo = Todo::from((0, "  // TODO -> This is a todo"));
+        let todo = Todo::from((0, "  // TODO -> This is a todo", "test.rs".to_string()));
 
         assert_eq!(todo.line_number, (1, 3));
         assert_eq!(todo.text, "This is a todo");
@@ -66,7 +72,7 @@ mod tests {
 
     #[test]
     fn test_todo_from_without_spacing() {
-        let todo = Todo::from((0, "//TODO: This is a todo"));
+        let todo = Todo::from((0, "//TODO: This is a todo", "test.rs".to_string()));
 
         assert_eq!(todo.line_number, (1, 1));
         assert_eq!(todo.text, "This is a todo");
@@ -75,7 +81,7 @@ mod tests {
 
     #[test]
     fn todo_from_removes_closing_tag() {
-        let todo = Todo::from((0, "<!-- TODO: This is a todo -->"));
+        let todo = Todo::from((0, "<!-- TODO: This is a todo -->", "test.rs".to_string()));
 
         assert_eq!(todo.line_number, (1, 1));
         assert_eq!(todo.text, "This is a todo");
