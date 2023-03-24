db808ddd5f67f783e9083a9828077640ff3d7701
Rickard Natt och Dag <rdag@invidi.com>
2023-03-24 19:16:47 +0100

    feat: add basic todo filtering

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{collections::HashMap, fs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = std::env::args().nth(1);
+    let needle = std::env::args().nth(2);
 
     if path.is_none() {
         println!("Usage: todo <path>");
@@ -15,6 +16,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let path = path.unwrap();
     let mut ok_files = 0;
+    let mut filtered_files = 0;
     let mut todos: HashMap<String, Vec<Todo>> = HashMap::new();
     let supported_filetypes = vec!["ts", "js", "tsx", "jsx", "vue", "html"];
 
@@ -35,13 +37,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let file_contents = fs::read_to_string(entry.path())?;
 
         // Skip files without TODOs
-        if contains_todo_type(&file_contents).is_err() {
-            ok_files += 1;
-            continue;
-        }
+        // if !vec!["//", "<!--"].contains(&file_contents) {
+        //     ok_files += 1;
+        //     continue;
+        // }
 
         for line in file_contents.lines().enumerate() {
             if contains_todo_type(line.1).is_ok() {
+                if needle.is_some() && !line.1.contains(needle.as_ref().unwrap()) {
+                    filtered_files += 1;
+                    continue;
+                }
+
                 todos
                     .entry(entry.path().display().to_string())
                     .or_default()
@@ -50,21 +57,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    println!(
-        "{ok_files} {} {}\n",
-        match ok_files {
-            1 => "file",
-            _ => "files",
-        },
-        "OK".green(),
-    );
+    match (ok_files, filtered_files) {
+        (0, 0) => println!("No TODOs found"),
+        (0, 1) => println!("1 file filtered"),
+        (1, 0) => println!("1 file OK"),
+        (1, 1) => println!("1 file OK / 1 file filtered"),
+        (_, 0) => println!("{} files OK", ok_files),
+        (_, 1) => println!("{} files OK / 1 file filtered", ok_files),
+        (0, _) => println!("{} files filtered", filtered_files),
+        (1, _) => println!("1 file OK / {} files filtered", filtered_files),
+        (_, _) => println!("{} files OK / {} files filtered", ok_files, filtered_files),
+    }
 
-    for file in todos {
-        for todo in file.1 {
+    println!("");
+
+    for (filename, todos) in todos {
+        for todo in todos {
             println!(
                 "{} {}",
                 todo,
-                file.0.replace(&path, "").bold().bright_black(),
+                filename.replace(&path, "").bold().bright_black(),
             );
         }
     }
